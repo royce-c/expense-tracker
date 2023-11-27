529fc3a28fbbe59b6a92d981dafa0c6b452384c4
r <coviello.royce@gmail.com>
2023-11-26 19:09:06 -0800

    fixed date formatting

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -1,28 +1,14 @@
 export default function formattedDate(date: Date): string {
-    const currentDate = new Date();
-    const seconds = Math.floor((currentDate.getTime() - date.getTime()) / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
-    const years = Math.floor(days / 364);
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+        timeZoneName: 'short',
+    };
 
-    if (years) {
-        return `${years}y`;
-    } else if (days) {
-        return `${days}d`;
-    } else if (hours || minutes || seconds) {
-        const formattedDateString = date.toLocaleString('en-US', {
-            month: 'short',
-            day: 'numeric',
-            year: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric',
-            hour12: true,
-            timeZoneName: 'short',
-        });
-
-        return formattedDateString.replace(',', '');
-    } else {
-        return 'Just now';
-    }
+    const formattedDateString = date.toLocaleString('en-US', options);
+    return formattedDateString;
 }
