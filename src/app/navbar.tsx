7338a0a4b9f280dd4076af904195019ea69be6ba
royce-c <114127058+royce-c@users.noreply.github.com>
2024-09-23 20:53:09 -0700

    fixed Invalid DOM property errors

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -12,7 +12,7 @@ export default async function NavBar({ className }: { className?: string }) {
       >
         <svg className="w-7 h-7 stroke-2" aria-label="Home" viewBox="0 0 26 26" role="img">
           <title>Home</title>
-          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
         </svg>
       </Link>
 
