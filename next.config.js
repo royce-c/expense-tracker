b6743d66fa2412031661d8e033ee2366f9f35911
royce-c <114127058+royce-c@users.noreply.github.com>
2024-10-03 02:46:25 -0700

    added google oauth

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "jsframework.s3.ca-central-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
 
-    images: {
-        remotePatterns: [
-            {
-              protocol: "https",
-              hostname: "avatars.githubusercontent.com",
-            },
-            {
-              protocol: "https",
-              hostname: "jsframework.s3.ca-central-1.amazonaws.com",
-            },
-          ],
-    },
-
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
